8445ef3eef430b36676fc21a68ae57c4e91464a0
ridwanraharja <9612621ridwan@gmail.com>
2025-09-27 19:33:25 +0700

    feat: setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import Navigation from "./components/Navbar";
-import DecorativeElements from "./components/DecorativeElements";
-import Hero from "./components/Hero";
-import CompanyLogos from "./components/CompanyLogos";
+import { Routes, Route } from "react-router";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import { useThemeStore } from "./store/themeStore";
 
 function App() {
   const { theme } = useThemeStore();
 
   return (
-    <div className={`${theme === "dark" ? "dark" : ""} min-h-screen bg-white dark:bg-slate-900 text-slate-900 dark:text-white relative overflow-hidden transition-colors duration-300`}>
-      <Navigation />
-      <DecorativeElements />
-      <Hero />
-      <CompanyLogos />
+    <div
+      className={`${
+        theme === "dark" ? "dark" : ""
+      } min-h-screen bg-white dark:bg-slate-900 text-slate-900 dark:text-white relative overflow-hidden transition-colors duration-300`}
+    >
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Hero from "../components/Hero";
+import CompanyLogos from "../components/CompanyLogos";
+import DecorativeElements from "../components/DecorativeElements";
+
+function Home() {
+  return (
+    <>
+      <DecorativeElements />
+      <Hero />
+      <CompanyLogos />
+    </>
+  );
+}
+
+export default Home;
